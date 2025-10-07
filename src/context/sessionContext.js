ed3ff0a6bcf5a486c15e0269376e3207e01378a2
João Pedro <joaopedromendesfernandes08@gmail.com>
2025-10-07 15:47:52 -0300

    Login de pacientes pronto, faltando alguns ajustes de interface.

---
+++ src/context/sessionContext.js
@@ -0,0 +1,26 @@
+'use client'
+import { createContext, useContext, useEffect, useState } from "react";
+import supabase from "../../supabase/client";
+
+export const SessionContext = createContext(null);
+
+export function SessionProvider ({children}) {
+    const [session, setSession] = useState(null);
+  useEffect(() => {
+    const getSession = async () => {
+      const { data } = await supabase.auth.getSession();
+      setSession(data.session)
+    };
+    getSession();
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => listener.subscription.unsubscribe();
+  }, []);
+  return (
+    <SessionContext.Provider value = {{ session, setSession }}>
+        {children}
+    </SessionContext.Provider>
+  )
+}
