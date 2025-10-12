88997b52cad8bc8310c5d20af3b52b258e61ba7b
João Pedro <joaopedromendesfernandes08@gmail.com>
2025-10-12 02:15:12 -0300

    Alterações no sistema de cadastro

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,61 +1,69 @@
-import { SessionContext } from "@/context/sessionContext";
 import supabase from "../../supabase/client";
-import { useContext } from "react";
 
-export async function signUp({ email, password, name, phone, role }) {
+export async function signUpPacientes({ 
+    email,
+    password, 
+    nome, 
+    telefone, 
+    data_nascimento,
+    cpf,
+    genero,
+    endereco,
+    numero,
+    complemento,
+    bairro,
+    cidade,
+    estado,
+    cep,
+    role 
+}) {
     try {
-        const { data, error } = await supabase.auth.signUp({
+        const { data: signUpData, error: signUpError } = await supabase.auth.signUp({
             email,
             password,
             options: {
-                data: { name, phone, role },
+                data: { name, telefone, role },
                 emailRedirectTo: 'http://localhost:3000/signIn',
                 shouldCreateUser: true
             }
 
         });
-        if (!data) {
-            console.error(error.message);
+        if (signUpError) {
+            console.error(signUpError.message);
             return null
         };
-        return data.user
+
+        const { data: insertData, error: insertError } = await supabase
+        .from('pacientes')
+        .insert([{
+            id: signUpData.user.id,
+            email,
+            nome,
+            telefone,
+            data_nascimento,
+            cpf,
+            genero,
+            endereco,
+            numero,
+            complemento,
+            bairro,
+            cidade,
+            estado,
+            cep
+        }])
+        .select();
+
+        if (insertError) {
+            console.error("error ao criar usuário.", insertError.message)
+            return null
+        }
+
+        return insertData[0];
     } catch (err) {
         console.error(err);
         return null
-
     }
 };
-
-export async function createUser({ session }) {
-    try {
-        if (!session || session.user) {
-            console.error("Usuário não encontrado erro: ()")
-            return null
-        }
-
-        const user = {
-            name: session.user.user_metadata.name,
-            email: session.user.email,
-            phone: session.user.user_metadata.phone,
-            role: session.user.user_metadata.role
-        };
-
-        if (!user.email || !user.role) {
-            console.log("Erro ao criar usuário (erro: 501)")
-            return null
-        }
-        const { error } = await supabase
-            .from('patients')
-            .insert({ name: user.name, email: user.email, phone: user.phone, role: user.role })
-
-        if (error) {
-            console.error("Erro ao adicionar usuário", error.message);
-        }
-
-    } catch (err) {
-        console.error("Não foi possível se conectar ao servidor", err.message)
-    }
-}
 
 export async function signIn(email, password) {
     try {
